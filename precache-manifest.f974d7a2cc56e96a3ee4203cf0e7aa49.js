8382d1c8d9aceb0cf371da5c4d5ba15c3a6c042f
mgrinko <glukym@gmail.com>
2019-12-12 18:34:39 +0200

    Updates

---
+++ precache-manifest.f974d7a2cc56e96a3ee4203cf0e7aa49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0267ebe9cd9992566d4e69f9c30a8d5f",
+    "url": "/react_static-list-of-goods/index.html"
+  },
+  {
+    "revision": "a765ac7d565b5cbe0b84",
+    "url": "/react_static-list-of-goods/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/react_static-list-of-goods/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "a765ac7d565b5cbe0b84",
+    "url": "/react_static-list-of-goods/static/js/main.27623947.chunk.js"
+  },
+  {
+    "revision": "44d924e48a9ebc848194",
+    "url": "/react_static-list-of-goods/static/js/runtime~main.1b375ebf.js"
+  }
+]);
